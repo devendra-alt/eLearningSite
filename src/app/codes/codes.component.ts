6570012c98aa1663c60ca3438e831f4a245faa33
Pratik <pratik.glsica19@gmail.com>
2020-09-18 22:25:24 +0530

    pie-plat update 3.0

---
+++ src/app/codes/codes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-codes',
+  templateUrl: './codes.component.html',
+  styleUrls: ['./codes.component.css']
+})
+export class CodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
