ee385713396dba2339dc3007b91c98a7c3fcf6f1
Shaurya Kajiwala <kajiwalashaurya29@gmail.com>
2020-09-19 11:49:37 +0530

    home page update 3.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { LoginComponent } from './login/login.component';
 import { MainheaderComponent } from './mainheader/mainheader.component';
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
